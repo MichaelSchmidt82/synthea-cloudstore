6afd16e52e3b923721465ea5aa1ec3228281f03f
Michael Schmidt <Michael.Schmidt@VUMC.org>
2021-10-12 10:51:58 -0500

    add exception handling

--- gs-etl/entrypoint.py
+++ gs-etl/entrypoint.py
@@ -1,42 +1,67 @@
 import os
+import sys
 import glob
 import time
-from google.cloud import storage
+from google.cloud import storage, exceptions
 
 
 def main():
     """
     Main entrypoint
     """
-
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(os.environ.get('BUCKET_NAME'))
-
+    #* Wait for synthea
     while True:
         if os.path.exists('/messages/synthea'):
             break
         time.sleep(2)
 
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(os.environ.get('BUCKET_NAME'))
+
+    try:
+        assert storage_client.lookup_bucket(bucket)
+    except AssertionError as exc:
+        print(f'Bucket {os.environ.get("BUCKET_NAME")} does not exist',
+              file=sys.stderr,
+              flush=True)
+        print(exc)
+        return 1
+    except exceptions.GoogleCloudError as exc:
+        print(exc)
+        return 1
+
     upload('/input', bucket)
+    return 0
 
 
 def upload(path, bucket):
     """
     Recursively copy a folder of files & folders to GCS.
-
     path should be a valid folder without a trailing slash
     """
     assert os.path.isdir(path)
     for local_file in glob.glob(path + '/**'):
+
+        #* local_file is a folder. Recurse into that folder
         if not os.path.isfile(local_file):
             upload(local_file, bucket)
             continue
+
+        #* local_file is a file.  Create a path and attempt to upload the file
         remote_path = os.path.join(os.environ.get('GCS_PATH'),
                                    local_file[1 + len('/input'):])
-        blob = bucket.blob(remote_path)
-        blob.upload_from_filename(local_file)
+
+        try:
+            blob = bucket.blob(remote_path)
+            blob.upload_from_filename(local_file)
+        except exceptions.GoogleCloudError:
+            print(f'failed to upload {local_file[len("/input"):]}',
+                  file=sys.stderr,
+                  flush=True)
+            continue
+
         print(f'uploaded {local_file[len("/input"):]}', flush=True)
 
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
